b1c6c732f27ee6583eb825f2bc7ca8f4d2d30268
iyaki <ivanyaki96@gmail.com>
2024-04-20 18:25:59 +0000

    Ajustes en BD

--- migrations/01-feeds.sql
+++ migrations/01-feeds.sql
@@ -1,14 +1,13 @@
 -- feeds definition
 
 CREATE TABLE feeds (
-	uri TEXT NOT NULL,
+	uri TEXT NOT NULL PRIMARY KEY,
 	title TEXT NOT NULL,
 	link TEXT,
 	last_update INTEGER NOT NULL,
 	trigger_hour INTEGER NOT NULL,
 	last_post_uri TEXT,
-	last_post_title TEXT,
-	CONSTRAINT feeds_pk PRIMARY KEY (uri)
+	last_post_title TEXT
 );
 
 CREATE INDEX feeds_trigger_hour_IDX ON feeds (trigger_hour);
--- migrations/02-subscriptions.sql
+++ migrations/02-subscriptions.sql
@@ -3,7 +3,9 @@
 CREATE TABLE subscriptions (
 	feed_uri TEXT NOT NULL,
 	email TEXT NOT NULL,
-	active INTEGER NOT NULL
+	active INTEGER NOT NULL,
+	FOREIGN KEY(feed_uri) REFERENCES feeds(uri),
+	UNIQUE(feed_uri, email)
 );
 
 CREATE INDEX subscriptions_feed_uri_IDX ON subscriptions (feed_uri);
