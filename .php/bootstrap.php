13441c1652850dfb94e8a23ae2139ede274abbde
iyaki <ivanyaki96@gmail.com>
2024-04-23 17:55:02 +0000

    Se agrega sentry para tracking de errores

--- .php/bootstrap.php
+++ .php/bootstrap.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 if (\file_exists(__DIR__ . '/../vendor/autoload.php')) {
     require __DIR__ . '/../vendor/autoload.php';
+
+    \Sentry\init([
+        'dsn' => 'https://c0548d393c74db8777af292c0a4cfcde@o249980.ingest.us.sentry.io/4507137129578496',
+    ]);
 } else {
     \trigger_error('composer autoload file not found. Please run `composer install`');
 }
-
-// TODO: Add Sentry
