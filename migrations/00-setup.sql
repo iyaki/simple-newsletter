b0ad1f9fffc7a56f765eb9a1b7b566b38e3885f1
iyaki <ivanyaki96@gmail.com>
2024-06-25 12:32:49 -0300

    Mejoras en la configuracion de la BD

--- migrations/00-setup.sql
+++ migrations/00-setup.sql
@@ -1,1 +1,8 @@
+PRAGMA journal_mode = WAL;
+PRAGMA synchronous = NORMAL;
+PRAGMA journal_size_limit = 67108864; -- 64 megabytes
+PRAGMA mmap_size = 134217728; -- 128 megabytes
+PRAGMA temp_store = MEMORY;
+PRAGMA cache_size = 2000;
+PRAGMA busy_timeout = 5000;
 PRAGMA foreign_keys = ON;
