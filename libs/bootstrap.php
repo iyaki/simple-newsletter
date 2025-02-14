2b67a8f0f41f54733c103c2e39df29153dbf9bbf
iyaki <ivanyaki96@gmail.com>
2025-02-14 02:27:39 +0000

    Mejoras menores en bootstrap.php

--- libs/bootstrap.php
+++ libs/bootstrap.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 (function () {
     if (! \file_exists(__DIR__ . '/../vendor/autoload.php')) {
-        \trigger_error('composer autoload file not found. Please run `composer install`');
+        if (basename($_SERVER["SCRIPT_FILENAME"], '.php') !== 'composer') {
+            \trigger_error('composer autoload file not found. Please run `composer install`');
+        }
 
         return;
     }
